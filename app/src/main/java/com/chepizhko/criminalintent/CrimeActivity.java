bc1bd5f6ff6d295388f6752240c09661779cc4b0
Ruslan Chepizhko <chepizhko.ruslan@gmail.com>
2017-12-18 12:43:42 +0200

    add class CrimeFragment

--- app/src/main/java/com/chepizhko/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/chepizhko/criminalintent/CrimeActivity.java
@@ -1,7 +1,9 @@
 package com.chepizhko.criminalintent;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class CrimeActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class CrimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
 
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
     }
+
 }
